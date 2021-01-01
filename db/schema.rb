3ce71922b84c8f055ceded41334c89da2a22d7e2
Pedro Ivo <pedryvo@gmail.com>
2021-01-01 09:24:03 -0300

    add event_date, event_hour and location to model campaign

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_003221) do
+ActiveRecord::Schema.define(version: 2021_01_01_122202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2021_01_01_003221) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "event_date"
+    t.string "event_hour"
+    t.string "location"
     t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
