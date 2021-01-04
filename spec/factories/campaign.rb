dbb0e3ea3e5e5e9aea6a3d133133eb147e84cb2f
Pedro Ivo <pedryvo@gmail.com>
2021-01-04 18:49:09 -0300

    fix campaigns controller, model and specs

--- spec/factories/campaign.rb
+++ spec/factories/campaign.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     title         { FFaker::Lorem.word }
     description   { FFaker::Lorem.sentence }
     user
+    status        { :pending }
+    location        { "#{FFaker::Address.city}, #{FFaker::Address.street_address}"}
+    event_date    { FFaker::Time.date }
+    event_hour    { rand(24).to_s }
   end
  end
  
